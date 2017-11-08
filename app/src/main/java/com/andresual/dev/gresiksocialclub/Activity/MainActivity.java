fb751c945c9c5d69e7a93a3af32ec0469b710336
andresual <andrean9103@gmail.com>
2017-11-08 19:01:36 +0700

    3 Changes

--- app/src/main/java/com/andresual/dev/gresiksocialclub/Activity/MainActivity.java
+++ app/src/main/java/com/andresual/dev/gresiksocialclub/Activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.andresual.dev.gresiksocialclub.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.andresual.dev.gresiksocialclub.R;
 
@@ -11,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSignup = (Button) findViewById(R.id.btn_signup);
+        Button btnSignin = (Button) findViewById(R.id.btn_signin);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SigninActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
